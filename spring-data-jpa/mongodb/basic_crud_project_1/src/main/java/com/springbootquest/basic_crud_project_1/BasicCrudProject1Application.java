e2914c0eaf7defd4ca2868ac67ac7d56e831a404
pdheeraj99 <pilladheeraj@gmail.com>
2025-05-09 10:11:27 +0530

    MongoDB Sample Project
    Basic CRUD Initial Setup

---
+++ spring-data-jpa/mongodb/basic_crud_project_1/src/main/java/com/springbootquest/basic_crud_project_1/BasicCrudProject1Application.java
@@ -0,0 +1,13 @@
+package com.springbootquest.basic_crud_project_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudProject1Application.class, args);
+	}
+
+}
---
+++ spring-data-jpa/mongodb/basic_crud_project_1/src/test/java/com/springbootquest/basic_crud_project_1/BasicCrudProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootquest.basic_crud_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
