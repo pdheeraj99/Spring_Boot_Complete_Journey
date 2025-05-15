61eacafa0e5fb3de72f2b0045d0cc4b911ea4361
pdheeraj99 <pilladheeraj@gmail.com>
2025-05-15 11:16:52 +0530

    Minor Project Starting...

---
+++ spring-mvc/web-app-minorproject4/src/main/java/com/learnspringmvc4/web_app_minorproject4/WebAppMinorproject4Application.java
@@ -0,0 +1,13 @@
+package com.learnspringmvc4.web_app_minorproject4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppMinorproject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppMinorproject4Application.class, args);
+	}
+
+}
---
+++ spring-mvc/web-app-minorproject4/src/test/java/com/learnspringmvc4/web_app_minorproject4/WebAppMinorproject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringmvc4.web_app_minorproject4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppMinorproject4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
