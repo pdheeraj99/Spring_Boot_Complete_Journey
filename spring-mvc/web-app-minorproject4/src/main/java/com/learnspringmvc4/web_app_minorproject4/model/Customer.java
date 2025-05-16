d1e28de4dbcd372b12ed7b38b2ce9c187c5e1dde
pdheeraj99 <pilladheeraj@gmail.com>
2025-05-16 19:29:21 +0530

    Web App Minor Project continue

---
+++ spring-mvc/web-app-minorproject4/src/main/java/com/learnspringmvc4/web_app_minorproject4/model/Customer.java
@@ -0,0 +1,69 @@
+package com.learnspringmvc4.web_app_minorproject4.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String city;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Customer(Integer id, String firstName, String lastName, String city) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
+
+    public Customer() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + "]";
+    }
+
+}
