c5800df66aa8f3b2a117c773d1a4de7793f2b0d6
pdheeraj99 <pilladheeraj@gmail.com>
2025-05-18 15:36:33 +0530

    deleted project in spring mvc and shifted to separate directory

--- spring-mvc/web-app-minorproject4/src/main/java/com/learnspringmvc4/web_app_minorproject4/repo/ICustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.learnspringmvc4.web_app_minorproject4.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.learnspringmvc4.web_app_minorproject4.model.Customer;
-
-public interface ICustomerRepo extends CrudRepository<Customer, Integer> {
-
-}
