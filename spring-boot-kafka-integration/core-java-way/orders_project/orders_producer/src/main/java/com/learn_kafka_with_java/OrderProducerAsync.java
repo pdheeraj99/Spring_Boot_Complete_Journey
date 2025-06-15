d8734dbdfb165b5f8b37f62f4205e78da9ebd2dc
pdheeraj99 <pilladheeraj@gmail.com>
2025-06-15 14:56:48 +0530

    Synchronous Comm

---
+++ spring-boot-kafka-integration/core-java-way/orders_project/orders_producer/src/main/java/com/learn_kafka_with_java/OrderProducerAsync.java
@@ -0,0 +1,5 @@
+package com.learn_kafka_with_java;
+
+public class OrderProducerAsync {
+
+}
--- spring-boot-kafka-integration/core-java-way/orders_project/orders_producer/src/main/java/com/learn_kafka_with_java/OrderProducerSync.java
+++ spring-boot-kafka-integration/core-java-way/orders_project/orders_producer/src/main/java/com/learn_kafka_with_java/OrderProducerSync.java
@@ -4,8 +4,9 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
-public class OrderProducer {
+public class OrderProducerSync {
 
     public static void main(String[] args) {
 
@@ -18,7 +19,20 @@ public class OrderProducer {
         ProducerRecord<String, Integer> record = new ProducerRecord<String, Integer>("OrderTopic", "Mac Book Pro", 10);
 
         try {
-            producer.send(record);
+            // 1. The send(record) method is asynchronous, but calling .get() makes it
+            // synchronous,
+            // 2. The RecordMetadata object contains metadata about the sent record, such as
+            // the partition and offset.
+            RecordMetadata recordMetadata = producer.send(record).get();
+
+            // Prints the partition number where the record was written.
+            System.out.println(recordMetadata.partition());
+
+            // Prints the offset of the record in the partition. The offset is a unique
+            // identifier for the record within the partition.
+            System.out.println(recordMetadata.offset());
+            System.out.println("Record Sent from here");
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
