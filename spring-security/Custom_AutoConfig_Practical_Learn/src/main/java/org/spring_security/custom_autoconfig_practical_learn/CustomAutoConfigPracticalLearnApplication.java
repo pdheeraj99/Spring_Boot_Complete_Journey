86a4530466fe64ea2e974b8326c0eca1edc0d724
pdheeraj99 <pilladheeraj@gmail.com>
2025-06-23 10:21:04 +0530

    AutoConfiguration Learning Example Start

---
+++ spring-security/Custom_AutoConfig_Practical_Learn/src/main/java/org/spring_security/custom_autoconfig_practical_learn/CustomAutoConfigPracticalLearnApplication.java
@@ -0,0 +1,13 @@
+package org.spring_security.custom_autoconfig_practical_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAutoConfigPracticalLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomAutoConfigPracticalLearnApplication.class, args);
+    }
+
+}
---
+++ spring-security/Custom_AutoConfig_Practical_Learn/src/test/java/org/spring_security/custom_autoconfig_practical_learn/CustomAutoConfigPracticalLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring_security.custom_autoconfig_practical_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAutoConfigPracticalLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
