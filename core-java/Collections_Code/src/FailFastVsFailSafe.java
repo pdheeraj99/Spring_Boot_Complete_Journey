1c0b9f0cedc60caa7e599ff010b5ea9649be47e4
pdheeraj99 <pilladheeraj@gmail.com>
2025-07-15 10:47:44 +0530

    Update FailFastVsFailSafe.java

--- core-java/Collections_Code/src/FailFastVsFailSafe.java
+++ core-java/Collections_Code/src/FailFastVsFailSafe.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-public class FailFastvsFailSafe {
+public class FailFastVsFailSafe {
 
     public static void main(String[] args) {
 
