fb65ec66744ce0362619e0c395a6e984f6a23e79
pdheeraj99 <pilladheeraj@gmail.com>
2025-07-24 22:53:22 +0530

    Added project

---
+++ spring-security/secure-ecommerce-api/src/main/java/com/ecom/secure/api/SecureEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.secure.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureEcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureEcommerceApiApplication.class, args);
+    }
+
+}
