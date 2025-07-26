6c394c35a410fabe83654dd4b745c82c61de99b9
pdheeraj99 <pilladheeraj@gmail.com>
2025-07-26 16:26:25 +0530

    Update SessionManagementFilterApplication.java

--- spring-security/both/session_management_filter/src/main/java/com/security/learn/session_management_filter/SessionManagementFilterApplication.java
+++ spring-security/both/session_management_filter/src/main/java/com/security/learn/session_management_filter/SessionManagementFilterApplication.java
@@ -3,8 +3,6 @@ package com.security.learn.session_management_filter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.*;
-
 // IMPORTANT POINTS
 // 1. SessionManagementFilter can be configured for either stateful or stateless behavior, but it enforces only one policy at a time for your application.
 // 2. You use it to set one rule for how your entire application should handle sessions.
