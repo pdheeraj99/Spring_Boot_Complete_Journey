6c773cd54d70aae4c20026ef57172829b57e9a4e
pdheeraj99 <pilladheeraj@gmail.com>
2025-07-30 12:17:27 +0530

    Starting Spring with Google OAuth 2.0

---
+++ spring-security/stateless/learn_springwithgoogleoauth/src/main/java/com/security/learn/learn_springwithgoogleoauth/LearnSpringwithgoogleoauthApplication.java
@@ -0,0 +1,13 @@
+package com.security.learn.learn_springwithgoogleoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringwithgoogleoauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringwithgoogleoauthApplication.class, args);
+    }
+
+}
---
+++ spring-security/stateless/learn_springwithgoogleoauth/src/test/java/com/security/learn/learn_springwithgoogleoauth/LearnSpringwithgoogleoauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.learn.learn_springwithgoogleoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringwithgoogleoauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
