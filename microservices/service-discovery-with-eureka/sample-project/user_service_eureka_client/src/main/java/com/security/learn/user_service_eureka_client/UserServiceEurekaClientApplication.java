a33cea24150365b5b5a51bfed200cec70a6fa43b
pdheeraj99 <pilladheeraj@gmail.com>
2025-07-31 20:41:15 +0530

    created user service eureka client

---
+++ microservices/service-discovery-with-eureka/sample-project/user_service_eureka_client/src/main/java/com/security/learn/user_service_eureka_client/UserServiceEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.security.learn.user_service_eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceEurekaClientApplication.class, args);
+    }
+
+}
---
+++ microservices/service-discovery-with-eureka/sample-project/user_service_eureka_client/src/test/java/com/security/learn/user_service_eureka_client/UserServiceEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.learn.user_service_eureka_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
